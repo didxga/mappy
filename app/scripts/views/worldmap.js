32682b3f442036bdd7a216536c84236a383a96d0
Carter <Carter@CN15752-D-1>
2013-12-19 09:43:09 +0800

    added bells and whistles

--- app/scripts/views/worldmap.js
+++ app/scripts/views/worldmap.js
@@ -5,32 +5,24 @@ define([
     'underscore',
     'backbone',
     'templates',
+	'../models/worldmap',
+	'../mapUtils',
     'jvectormapworlden',
     'bowerjvectormap'
-], function ($, _, Backbone, JST, Jvectormap) {
+], function ($, _, Backbone, JST, Model, MapUtils) {
     'use strict';
 
     var WorldmapView = Backbone.View.extend({
         template: JST['app/scripts/templates/worldmap.hbs'],
+	    model: new Model(),
         render: function() {
-            console.log(this.$el);
-            this.$el.vectorMap({
-                map: 'world_mill_en',
-                scaleColors: ['#C8EEFF', '#0071A4'],
-                normalizeFunction: 'polynomial',
-                hoverOpacity: 0.7,
-                hoverColor: false,
-                markerStyle: {
-                    initial: {
-                        fill: '#F8E23B',
-                        stroke: '#383f47'
-                    }
-                },
-                backgroundColor: '#383f47',
-                markers: [
-                    {latLng: [37.0000, 127.5000], name: 'Vatican City', scale:[5,20],value:19, fill: 'black'}
-                ]
-            });
+	        var _self = this;
+	        this.model.fetch({"success": function(resp){
+		        MapUtils.drawMap(_self.$el, resp.toJSON().data, function(code) {
+			        console.log(code);
+		        });
+	        }, "error":function(e,response){console.log(response.responseText);}});
+
         }
     });
 
